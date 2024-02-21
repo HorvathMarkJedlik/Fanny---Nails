3a6d51e7a63ed69a470ac67ddefa3a813a32fbda
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-02-21 09:10:19 +0100

    kész a hibaüzenet prompt

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ $(".inner-switch").on("click", function() {
     }
 });
 
+function validateForm() {
+    var name = document.forms[0]["name"].value;
+    var email = document.forms[0]["email"].value;
+    var date = document.forms[0]["date"].value;
+    var message = document.forms[0]["text"].value;
 
+    if (name === "" || email === "" || date === "" || message === "") 
+    {
+        alert("Minden mezőt ki kell tölteni!");
+    } 
+    else 
+    {
+
+        alert("Foglalás elküldve!");
+    }
+}
