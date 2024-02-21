abd84c167d1ddc6d19a2487d786f07318212ffd1
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-02-21 09:16:21 +0100

    foglalas menu hitelesitese

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ $(".inner-switch").on("click", function() {
     }
 });
 
-function validateForm() {
+function Hitelesites() {
     var name = document.forms[0]["name"].value;
     var email = document.forms[0]["email"].value;
     var date = document.forms[0]["date"].value;
-    var message = document.forms[0]["text"].value;
 
-    if (name === "" || email === "" || date === "" || message === "") 
+    if (name === "" || email === "" || date === "") 
     {
         alert("Minden mezőt ki kell tölteni!");
     } 
